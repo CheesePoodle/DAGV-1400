0ed3d6a998da2627f08a2f9d4733f80e0bc4aa21
Reina Lizaso <reinaalizaso@gmail.com>
2025-02-11 14:22:08 -0700

    Added Froggy animations and controls!

---
+++ DAGV Game/Assets/Scenes/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //Cache the animator component attached to CharacterArt
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        //running and idling
+        if(Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("RunTrigger");
+
+        }
+        else
+        {
+            animator.SetTrigger("IdleTrigger");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("DoubleJumpTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("HitTrigger");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("FallTrigger");
+        }
+
+        //wall jumping
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Wall Jump");
+        }
+    }
+}
