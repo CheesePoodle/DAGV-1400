9f05e92e495ee9439c1ca24a94866039d574a84b
Reina Lizaso <reinaalizaso@gmail.com>
2025-03-05 17:31:37 -0700

    Fixed UI issues and added score counter with banana money

---
+++ DAGV Game/Assets/Scenes/Scripts/SimpleIntData.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Single Variables/ SimpleIntData")]
+public class SimpleIntData : ScriptableObject
+{
+    public int value;
+
+    void Start()
+    {
+        value = 0;
+    }
+    public void UpdateValue(int amount)
+    {
+        value += amount;
+    }
+    
+    public void SetValue(int amount)
+    {
+        value = amount;
+    }
+}
---
+++ DAGV Game/Assets/Scenes/Scripts/SimpleTextMeshProBehaviour.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Globalization;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class SimpleTextMeshProBehaviour : MonoBehaviour
+{
+    private TextMeshProUGUI textObj;
+    public SimpleIntData dataObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textObj = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateWithIntData();
+    }
+    void UpdateWithIntData()
+    {
+        textObj.text = "Score: " + dataObj.value.ToString(CultureInfo.InvariantCulture);
+    }
+}
